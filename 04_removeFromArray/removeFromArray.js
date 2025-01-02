2367d3bda5f73102e6adf705b0a14c43e9d1c772
k4ileb <40836142+k4iLeb@users.noreply.github.com>
2025-01-02 19:37:55 +0200

    Finish 4th exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (arr, ...args) {
+  // const args = Array.prototype.slice.call(arguments, 1);
+  console.log(args);
 
+  // for (let num of args) {
+  //   arr = arr.filter((x) => x !== num);
+  // }
+
+  arr = arr.filter((item) => !args.includes(item));
+  console.log(arr);
+
+  return arr;
+};
+removeFromArray([1, 2, 3, 4], 1, 2, 3, 4);
 // Do not edit below this line
 module.exports = removeFromArray;
