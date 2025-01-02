5784d10b9ca0dd3af337ab15ef2ed0ace4bae194
k4ileb <40836142+k4iLeb@users.noreply.github.com>
2025-01-03 01:54:55 +0200

    Finish 5th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (a >= 0 && b >= 0 && Number.isInteger(a) && Number.isInteger(b)) {
+    const [min, max] = [a, b].sort((a, b) => a - b);
+    let arr = [];
+    for (let i = min; i <= max; i++) {
+      arr.push(i);
+    }
 
+    let count = 0;
+    arr.forEach((x) => (count += x));
+    console.log(count);
+
+    return count;
+  } else return "ERROR";
 };
 
 // Do not edit below this line
