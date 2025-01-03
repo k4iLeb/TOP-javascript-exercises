860a52f11b0f8480d42b403a5b4db78c39046d1b
k4ileb <40836142+k4iLeb@users.noreply.github.com>
2025-01-03 20:39:40 +0200

    Finish 8th exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return (result = a + b);
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return (result = a - b);
 };
 
-const sum = function() {
-	
+const sum = function (...args) {
+  let nums = args.flat();
+  let count = 0;
+  for (let num of nums) {
+    count += num;
+  }
+
+  return count;
+};
+const multiply = function (...args) {
+  let nums = args.flat();
+  let count = 1;
+  for (let num of nums) {
+    count *= num;
+  }
+  return count;
 };
 
-const multiply = function() {
-
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function (num) {
+  let count = 1;
+  for (let i = num; i > 0; i--) {
+    count *= i;
+  }
+  return count;
 };
 
 // Do not edit below this line
@@ -29,5 +43,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
