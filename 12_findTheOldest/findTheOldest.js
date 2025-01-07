7bfb0c7eefdc4f06e6a014b06080024d0c60cacf
k4ileb <40836142+k4iLeb@users.noreply.github.com>
2025-01-08 01:43:11 +0200

    Finish 12th exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  const updatedPeople = arr
+    .map((person) => {
+      if (!person.yearOfDeath) {
+        person.yearOfDeath = new Date().getFullYear();
+      }
+      return person;
+    })
+    .map((person) => {
+      return { ...person, lived: person.yearOfDeath - person.yearOfBirth };
+    });
 
+  updatedPeople.sort((a, b) => {
+    return b.lived - a.lived;
+  });
+  return updatedPeople[0];
 };
 
 // Do not edit below this line
