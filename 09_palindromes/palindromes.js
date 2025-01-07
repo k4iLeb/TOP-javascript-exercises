bfcc49729909122b3a5cf3b0c5e3fe1cd63b6d64
k4ileb <40836142+k4iLeb@users.noreply.github.com>
2025-01-08 00:37:46 +0200

    Finish 9th exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let regex = /[a-z0-9]/;
+  str = str
+    .toLowerCase()
+    .split("")
+    .filter((letter) => regex.test(letter))
+    .join("");
+  let testStr = str.split("").reverse().join("");
 
+  return str === testStr;
 };
+
+// console.log(palindromes("A car, a man, a maraca"));
+// console.log(palindromes("Animal loots foliated detail of stool lamina"));
 
 // Do not edit below this line
 module.exports = palindromes;
