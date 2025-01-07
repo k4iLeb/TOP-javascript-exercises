e38d5e5d3867285b345dd7287151bc5ab1b79e58
k4ileb <40836142+k4iLeb@users.noreply.github.com>
2025-01-08 00:57:17 +0200

    Finish 10th exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (arg) {
+  if (arg == 0) return 0;
+  if (arg < 0) return "OOPS";
+  let num = +arg;
+  let arr = [1, 1];
+  for (let i = 2; i < num; i++) {
+    arr.push(arr[i - 1] + arr[i - 2]);
+  }
+  return arr[num - 1];
 };
+
+fibonacci(10);
 
 // Do not edit below this line
 module.exports = fibonacci;
